010942c449e17c10c6a891b78867a6a5c83d8c75
stefanocasafranca <scasafrancal01@gmail.com>
2025-03-30 17:50:02 -0500

    Working Prototype of the checklist. Search Mode Added.

---
+++ Project1-ToDoList-Casafranca/ToDoCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoCell.swift
+//  Project1-ToDoList-Casafranca
+//
+//  Created by Stefano Casafranca on 3/30/25.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
